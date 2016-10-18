0ac6be418a74b255d89fc02f2d019224c5777724
Andrew Briscoe <admin@pandasportal.net>
2016-10-18 13:59:47 +0800

    Added script to run experiments, eventually.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # cits4419-dsdv
-Modified code to experiment with the DSDV protocol for use with the ns-3 simulator
+Modified code to experiment with the DSDV protocol for use with the ns-3 simulator. All code should be slight modifications of unmodified-dsdv.cc or the protocols
+## Source code
+- unmodified-dsdv.cc
+- original-protocols/
+-- TO ADD...
+## Run experiments
+- experiment.sh will run all the simulations for all parameters and save the data
+## Experiment 1
+- full-updates-only-dsdv.cc
+- modified-protocols/
+-- full-updates-only-dsdv-protocol.cc
+-- full-updates-only-dsdv-protocol.h
+## Experiment 2
+- disable-nodes-updating-dsdv.cc [number of non-updating nodes]
+## Experiment 3
+- ignore-routetable-column-dsdv.cc [probability]
+## Experiment 4
+- ignore-sequence-number-dsdv.cc
---
+++ experiment.sh
@@ -0,0 +1,32 @@
+## WARNING: Not yet tested
+declare -a experiment2 =("5" "10" "15")
+declare -a experiment3 =("0.4" "0.5" "0.6")
+
+echo "Starting experiment 1"
+echo "Starting simulation with unmodified DSDV protocol"
+# this runs unmodified-dsdv.cc and outputs to unmodifiedDSDV.dat instead 
+# of screen
+./../waf --run "unmodified-dsdv" > unmodifiedDSDV.dat 2>&1
+# reads in a bunch of output from the simulation, and organises the data needed
+## python parse-data.py -filein unmodifiedDSDV.dat > unmodifiedDSDV.stat
+# show the progress (update next line accordingly)
+## tail -n 13 unmodifiedDSDV.dat
+## cat unmodifiedDSDV.stat
+./../waf --run "full-updates-only-dsdv" > fullUpdatesDSDV.dat 2>&1
+echo "Starting experiment 2"
+for i in "${experiment2[@]}"
+do
+  echo "Experiment 2: disabled updating for ${i} nodes"
+  ./../waf --run "disable-nodes-updating-dsdv --num-disabled-nodes ${i}" > disable-${i}-nodes-updating-dsdv.dat 2>&1
+  #python parse-data.py --filein disable-${i}-nodes-updating-dsdv.dat >  disable-${i}-nodes-updating-dsdv.stat
+  #tail -n [num of relevant lines] [filename.dat]
+done
+echo "Starting experiment 3"
+for i in "${experiment3[@]}"
+do
+  echo "Experiment 3: Ignore Route Table Column (probability ${i})"
+  ./../waf --run "ignore-routetable-column-dsdv --ignore-probability ${i}" > ignore-${i}-routetable-column-dsdv.dat 2>&1
+done
+echo "Starting experiment 4: Ignoring sequence numbers in DSDV"
+./../waf --run "ignore-sequence-number-dsdv" > ignore-sequence-number-dsdv.dat 2>&1
+echo "Done!"
