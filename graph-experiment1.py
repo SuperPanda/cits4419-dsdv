c0f75fe1bc46b4e1590dba90188fc45f03a1b0a9
Andrew Briscoe <admin@pandasportal.net>
2016-10-23 04:43:57 +0800

    Data analysis

---
+++ graph-experiment1.py
@@ -0,0 +1,35 @@
+#import Gnuplot
+from matplotlib import pyplot as plt
+import numpy as np
+import sys
+dt = np.dtype([('cbrNodes',int),('nodeSpeed',float),('throughput',float)])
+a = []
+for i in range(1,len(sys.argv)):
+  print("loading " + sys.argv[i] + "...")
+  # numCBRNodes, nodeSpeed, throughput
+  b = np.genfromtxt(open(sys.argv[i],'r'),delimiter=",",skiprows=1, dtype=dt)
+  a.append((b['cbrNodes'].item(0),b['nodeSpeed'].item(0),b['throughput'].item(0)))
+list.sort(a)
+print(a)
+setOfNodeSpeeds = set([a1[1] for a1 in a])
+
+# Start plotting
+counter = 0
+combinations = ['ro', 'go', 'bo', 'rx', 'gx', 'bx']
+plt.title('Experiment 1: Throughput of unmodified DSDV protocol')
+plt.xlabel('Number of nodes generating CBR traffic')
+plt.ylabel('Throughput (pkts rx / pkts tx)')
+plt.axis([0,35,0,1.0])
+for nodeSpeed in setOfNodeSpeeds:
+  print(nodeSpeed)
+  # add subplot for each node speed
+  x = []
+  y = []
+  for item in [a1 for a1 in a]:
+    if nodeSpeed == item[1]:
+      x.append(item[0])
+      y.append(item[2])
+  plt.plot(x,y,combinations[counter%len(combinations)])
+  counter+=1
+  print([x, y])
+plt.show()
