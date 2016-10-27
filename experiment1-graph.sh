0caf86466fc9812a4796a63ae864e252bd8982a1
Andrew Briscoe <admin@pandasportal.net>
2016-10-28 04:56:53 +0800

    Renamed stats folder for experiment 1

--- experiment1-graph.sh
+++ experiment1-graph.sh
@@ -1,1 +1,1 @@
-python experiment1-graph-compare.py "Throughput of unmodified DSDV protocol" baseline-stats "Throughput of DSDV protocol with only full updates" experiment1-full-update-stats
+python experiment1-graph-compare.py "Throughput of unmodified DSDV protocol" baseline-stats "Throughput of DSDV protocol with only full updates" experiment1-stats
