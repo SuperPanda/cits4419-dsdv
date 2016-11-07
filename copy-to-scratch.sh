a53383f606f13ba76afea659a97f92cafbf316a7
21332512 <21332512@ecm-csse2125-l.uniwa.uwa.edu.au>
2016-11-07 21:12:58 +0800

    Added copy utility

---
+++ copy-to-scratch.sh
@@ -0,0 +1,10 @@
+scratchLocation=$1
+
+cp -R experiment1-full-updates-dsdv $scratchLocation/
+cp -R experiment2-disable-nodes-updating-dsdv $scratchLocation/
+cp -R experiment3-randomly-incomplete-routing-tables $scratchLocation/
+cp -R experiment4-ignore-seq-numbers $scratchLocation/
+cp -R experiment5-optimal-update-time $scratchLocation/
+cp *experiment.sh $scratchLocation/
+
+
